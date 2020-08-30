6ccc3a0a347e660dd7fee4b1c24908fd73e68fb3
Marja Satukangas <marja.satukangas@metropolia.fi>
2020-08-30 23:23:21 +0300

    kurssitiedot added

---
+++ palautusrepositorio/osa2/kurssitiedot/src/components/Course.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const Header = ({ course }) => {
+    return (
+      <h1>{course.name}</h1>
+    )
+  }
+  
+  const Total = ({ course }) => {
+    console.log(course.parts)
+    const sum = course.parts.reduce((total, part) => total + part.exercises, 0)
+    return(
+      <p>Number of exercises {sum}</p>
+    ) 
+  }
+  
+  const Part = (props) => {
+    return (
+      <p>
+        {props.part.name} {props.part.exercises}
+      </p>    
+    )
+  }
+  
+  const Content = ({ course }) => {
+    return (
+      course.parts.map ((part, i) => <Part key={part.id} part={part} />)
+    )
+  }
+  
+  const Course = ({ course }) => {
+    return (
+      <div>
+        <Header course={course}/>
+        <Content course={course}/>
+        <Total course={course}/>
+      </div>
+    )
+  }
+
+  export default Course
