fc248dbf16f39ed0759402e603a9ed4065d0cfc2
Marja Satukangas <marja.satukangas@metropolia.fi>
2020-10-25 21:59:29 +0200

    2.6.

---
+++ palautusrepositorio/osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas', id: 1 }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addNewName = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      name: newName,
+      id: persons.length + 1,
+    }
+
+    setPersons(persons.concat(noteObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewName}>
+        <div>
+          name: 
+          <input
+            value={newName}
+            onChange={handleNameChange} 
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person =>
+          <li key={person.id}>{person.name}</li>
+        )}
+      </ul>
+    </div>
+  )
+
+}
+
+export default App
